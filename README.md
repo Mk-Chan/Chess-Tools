20ac4c258d17ecbe7d765c3589efd94d7e972e65
Manik Charan <mkchan2951@gmail.com>
2016-12-23 23:21:57 +0530

    Modified the epd tool to use the python-chess module

--- README.md
+++ README.md
@@ -2,12 +2,12 @@
 A collection of tools to help with chess programming.
 
 ## Current List
-* Chess-Perft
+* perft
 
 	A very fast perft tool derived from WyldChess(using Pradyumna Kannan's magic bitboards) that can be used to count leaf nodes, captures, enpassants, castles and promotions.
 	It can also be used to calculate split(divide) counts from the root position.
 
-* EPD-Test
+* epd2uci
 
 	A script that runs a chess engine(UCI only at the moment) for the given number of seconds over
-	a file containing a list of EPDs and prints results. Requires the pexpect module.
+	a file containing a list of EPDs and prints results. Requires the python-chess module.
---
+++ epd2uci.py
@@ -0,0 +1,112 @@
+"""
+ A free tool to test UCI engines over epd collections using the python-chess module
+ Copyright (C) 2016  Manik Charan
+
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program. If not, see <http://www.gnu.org/licenses/>.
+"""
+import sys
+import getopt
+import chess.uci
+
+def print_help():
+    print("Usage:")
+    print("python3 epd2uci.py -e <engine> -f <epd-collection> -t <time-per-position>")
+
+try:
+    opts, args = getopt.getopt(sys.argv[1:], "hf:e:t:")
+except:
+    print_help()
+    sys.exit(1)
+
+params_left = 3
+for opt, arg in opts:
+    if opt == '-e':
+        engine = arg
+        params_left -= 1
+    elif opt == '-f':
+        epd_file = arg
+        params_left -= 1
+    elif opt == '-t':
+        time_per_pos = float(arg)
+        params_left -= 1
+    elif opt == '-h':
+        print_help()
+        sys.exit(0)
+
+if params_left > 0:
+    print_help()
+    sys.exit(1)
+
+board = chess.Board()
+
+epd_file = open(epd_file, 'r')
+engine = chess.uci.popen_engine(engine)
+engine.uci()
+
+successes = 0
+total = 0
+failed_epds = []
+for epd in epd_file:
+    ops = board.set_epd(epd)
+    total += 1
+
+    print(ops.get("id"))
+    bms = ops.get("bm")
+    ams = ops.get("am")
+
+    if bms != None:
+        m_list = bms
+    elif ams != None:
+        m_list = ams
+
+    ms = ""
+    for m in m_list:
+        ms += str(m) + " "
+
+    if bms != None:
+        print("Best move(s): " + ms[:-1])
+    elif ams != None:
+        print("Avoid move(s): " + ms[:-1])
+
+    engine.position(board)
+    res = engine.go(movetime=(time_per_pos * 1000))
+    if bms != None:
+        fail = True
+        for bm in bms:
+            if res.bestmove == bm:
+                fail = False
+                break
+    elif ams != None:
+        fail = False
+        for am in ams:
+            if res.bestmove == am:
+                fail = True
+                break
+    if (fail):
+        print("Failed")
+        print("Engine move: " + str(res.bestmove))
+        failed_epds.append(epd)
+    else:
+        print("Succeeded")
+        print("Engine move: " + str(res.bestmove))
+        successes += 1
+
+    print("Current success rate: " + str(successes * 100.0 / total) + "%")
+    print()
+
+print("Success rate: " + str(successes * 100.0 / total) + "%")
+print("Failed: " + (total - successes))
+print("Failed EPDs:")
+for epd in failed_epds:
+    print(epd)
